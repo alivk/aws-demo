150aa64a3372e956d04cffd761ca7dfba4f9fc59
alivk <86346768+alivk@users.noreply.github.com>
2024-01-06 10:57:38 +0800

    Update demo-vpc-ipam.sh

--- cloudformation/demo-vpc-ipam.sh
+++ cloudformation/demo-vpc-ipam.sh
@@ -2,20 +2,22 @@
 
 # Step 01: Start the timer
 
-echo "STEP01 - Starting the script..."
 start_time=$(date +%s)
+stack_name="demo-vpc-ipam"
+echo "STEP01 - Starting the stack "$stack_name" creation ..."
+
 
 # Step 02: Downloading the CloudFormation template
 
 echo "STEP02 - Downloading the CloudFormation template..."
-curl -O https://raw.githubusercontent.com/alivk/aws-demo/main/cloudformation/demo-vpc-ipam.yml
+curl -O https://raw.githubusercontent.com/alivk/aws-demo/main/cloudformation/"$stack_name".yml
 
 # Step 03: Creating the CloudFormation stack
 
 echo "STEP03 - Creating the CloudFormation stack..."
 aws cloudformation create-stack \
-  --stack-name demo-vpc-ipam \
-  --template-body file://demo-vpc-ipam.yml \
+  --stack-name "$stack_name" \
+  --template-body file://"$stack_name".yml \
   --capabilities CAPABILITY_IAM \
   --parameters \
   ParameterKey=TagEnvironment,ParameterValue=Production \
@@ -27,9 +29,9 @@ aws cloudformation create-stack \
 echo -n "STEP04 - Waiting for CloudFormation stack creation to complete... "
 while true; do
   echo -n "STEP05 - Script is running in the background... "
-  aws cloudformation wait stack-create-complete --stack-name demo-vpc-ipam &> /dev/null
+  aws cloudformation wait stack-create-complete --stack-name "$stack_name" &> /dev/null
   if [ $? -eq 0 ]; then
-    echo "STEP06 - The CloudFormation Stack is Ready!!\n\n!"
+    echo "STEP06 - The CloudFormation Stack "$stack_name" is Ready!!\n\n!"
     break
   else
     echo -n "."
@@ -39,4 +41,4 @@ done
 
 end_time=$(date +%s)
 execution_time=$((end_time - start_time))
-echo "STEP06 - Script completed, Total execution time: ${execution_time} seconds"
+echo "STEP06 - Stack "$stack_name" completed, Total execution time: ${execution_time} seconds"
